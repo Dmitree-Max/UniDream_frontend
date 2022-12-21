ec55f522147c7090748c0d67204b91f5d2981498
Dmitree-Max <d.s.maximenko@gmail.com>
2022-12-22 01:15:08 +0300

    Created table

---
+++ src/universitiesTable.js
@@ -0,0 +1,52 @@
+import React, { useMemo, useEffect, useState } from 'react';
+import axios from 'axios';
+import MaterialReactTable from 'material-react-table';
+
+
+//nested data is ok, see accessorKeys in ColumnDef below
+
+
+const Example = () => {
+  const [data, setData] = useState([]);
+  const URI = "http://localhost:8080/v0/universities"
+
+  useEffect(() => {
+    axios.get(URI, {headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json',
+    }})
+    .then(response => setData(response.data))
+  }, [])
+
+  //should be memoized or stable
+  const columns = useMemo(
+    () => [
+      {
+        accessorKey: 'name',
+        header: 'University',
+      },
+      {
+        accessorKey: 'city',
+        header: 'City',
+      },
+      {
+        accessorKey: 'country',
+        header: 'Country',
+      },
+      {
+        accessorKey: 'students',
+        header: 'Students',
+      },
+      {
+        accessorKey: 'major_field',
+        header: 'Major field',
+      },
+    ],
+    [],
+  );
+
+  return <MaterialReactTable columns={columns} data={data} />;
+};
+
+
+export default Example;
